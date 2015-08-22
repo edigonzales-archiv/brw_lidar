d3128853e0454d78f6abe985c6228571c5488536
Stefan Ziegler <edi.gonzales@gmail.com>
2015-08-22 15:52:02 +0200

    backup

--- verification.py
+++ verification.py
@@ -104,17 +104,50 @@ def main():
         print cmd
         #os.system(cmd)
 
-        # Read the json dumps and put them in a map/container.
+        # Read the json dumps and put the result in dicts.
         map_lv03 = json_to_map(info_file_lv03)
         map_lv95 = json_to_map(info_file_lv95)
 
         print map_lv95
 
-
+        # Loop through dicts and compare the points.
+        # for-for... not very smart.
         for i in map_lv03:
-            #print i, ' corresponds to ', map_lv03[i]
+            gpstime_lv03 = map_lv03[i]['GpsTime']
+            intensity_lv03 = map_lv03[i]['Intensity']
+            classification_lv03 = map_lv03[i]['Classification']
+
             x_lv03 = map_lv03[i]['X']
-            print x_lv03
+            y_lv03 = map_lv03[i]['Y']
+            z_lv03 = map_lv03[i]['Z']
+            print gpstime_lv03
+
+            for j in map_lv95:
+                gpstime_lv95 = map_lv95[j]['GpsTime']
+                intensity_lv95 = map_lv95[j]['Intensity']
+                classification_lv95 = map_lv95[j]['Classification']
+
+                x_lv95 = map_lv95[i]['X']
+                y_lv95 = map_lv95[i]['Y']
+                z_lv95 = map_lv95[i]['Z']
+
+                # Try to identify the same point in LV03 and LV95.
+                # Since there is no real unique identifier (?) we use different
+                # attributes (and even the heigth).
+                # If we found the same point (actually it should be one point only!)
+                # we can transform LV03 to LV95 and compare the coordinates.
+                if gpstime_lv03 == gpstime_lv95 and intensity_lv03 == intensity_lv95 and classification_lv03 == classification_lv95 and z_lv03 == z_lv95:
+point_lv03 = ogr.Geometry(ogr.wkbPoint)
+point_lv03.AddPoint(x, y)
+
+# We need the very same point in EPSG:2056.
+# We have to use the same transformation method as we used
+# for transforming the lidar stuff.
+point_lv95 = lv03_to_lv95(point_lv03)
+print point_lv95
+
+
+
 
 
 
